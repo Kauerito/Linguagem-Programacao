984b5e4b901c3256b19e40c2f9eaa8456bcc9844
Kadu.u <78941992+kadukoviana@users.noreply.github.com>
2021-02-24 20:54:15 -0400

    Update
    Atualizei as informações do código do pedido 001

--- Código de Coleção de Dados.py
+++ Código de Coleção de Dados.py
@@ -10,8 +10,12 @@ se  iniciar  ==  '3' :
     pedir  =  input ( ' \ 033 [1; 39mQual lanche voce quer ??' )
 
     if  pedir  ==  '001' : 
-        detalhe1 = DetalhePedido ( 'Código pedido: 1345' , 'Nome do lanche: Burgão' , 'Código Produto: 001' , 'Quantidade: 01' , 'O lanche é R $ 12,00' , 'Taxa de Entrega: R $ 0, 00 ' , ' Valor final: R $ 12,00 ' )
-        detalhe1 . exibirDetalhes ()
+        detalhe1 = [ 'Código pedido: 1345' , 'Nome do lanche: Burgão' , 'Código Produto: 001' , 'Quantidade: 01' , 'O lanche é R $ 12,00' , 'Taxa de Entrega: R $ 0, 00 ' , ' Valor final: R $ 12,00 ' ]
+        detalhe1 .append('Valor Final: R$ 12,00')
+        detalhe1 .pop(6)
+        detalhe1 .insert(5 ,'Taxa de entrega: R $ 0,00')
+        
+        print(detalhe1)
         intervalo
   
     if  pedir  ==  '002' :
@@ -20,4 +24,4 @@ se  iniciar  ==  '3' :
       detalhe2 . pop ( 5 )
       detalhe2 . inserir ( 5 , 'Taxa de entrega: R $ 0,00' )
       imprimir ( detalhe2 )
-      intervaloSepa
+      intervalo
